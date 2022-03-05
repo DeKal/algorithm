08edf52c00fde1395498c6ba53e6aa786e5b4444
Phat Ho <hohuuphat22@gmail.com>
2022-03-05 12:50:42 +0700

    Refactor leetcode/pascalsTriangle.

--- leetcode/pascalsTriangle.java
+++ leetcode/pascalsTriangle.java
@@ -3,19 +3,21 @@
 class pascalsTriangle {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> pascalTriangle = new ArrayList<List<Integer>>();
-        for (int i = 0; i<numRows; ++i) {
+
+        List<Integer> firstRow = new ArrayList<Integer>();
+        firstRow.add(1);
+        pascalTriangle.add(firstRow);
+
+        for (int i = 1; i<numRows; ++i) {
             List<Integer> newRow = new ArrayList<Integer>();
-            if (i == 0) {
-                newRow.add(1);
 
-            } else {
-                newRow.add(1);
+            newRow.add(1);
 
-                for (int j=1; j < i; ++j) {
-                    newRow.add(pascalTriangle.get(i-1).get(j-1) + pascalTriangle.get(i-1).get(j));
-                }
-                newRow.add(1);
+            for (int j=1; j < i; ++j) {
+                newRow.add(pascalTriangle.get(i-1).get(j-1) + pascalTriangle.get(i-1).get(j));
             }
+            newRow.add(1);
+
             pascalTriangle.add(newRow);
         }
         return pascalTriangle;
